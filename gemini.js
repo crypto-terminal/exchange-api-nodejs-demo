3f330147ac6219c04dd33505e43dff7ca74af716
Yumin Gui <yumindev@gmail.com>
2022-06-11 15:30:11 -0700

    gemini multiple api calls

--- gemini.js
+++ gemini.js
@@ -4,41 +4,56 @@ import crypto from "crypto";
 const BASE_URL = "https://api.gemini.com";
 
 const PATHS = {
-    USER_ACCOUNT: "/v1/account",
+    ACCOUNT_INFO: "/v1/account",
+    ACCOUNT_BALANCES: "/v1/balances",
 };
 
 const API_KEY = process.env.GEMINI_API_KEY;
 const API_SECRET = process.env.GEMINI_API_SECRET;
 
-const FULL_ENDPOINT = BASE_URL + PATHS.USER_ACCOUNT;
+const getNonce = () => String(Math.floor(Date.now() / 1000) * 1000);
 
+const getHeaders = (path) => {
+    const nonce = getNonce();
+    const payload = {
+        request: path,
+        nonce,
+    };
 
-const nonce = String(Math.floor(Date.now() / 1000) * 1000);
-const payload = {
-    request: "/v1/account",
-    account: "primary",
-    nonce,
-};
+    const base64payload = Buffer.from(JSON.stringify(payload)).toString("base64");
+    const signature = crypto.createHmac("sha384", API_SECRET).update(base64payload).digest("hex");
+    const headers = {
+        "Content-Type": "text/plain",
+        "Content-Length": "0",
+        "X-GEMINI-APIKEY": API_KEY,
+        "X-GEMINI-PAYLOAD": base64payload,
+        "X-GEMINI-SIGNATURE": signature,
+        "Cache-Control": "no-cache",
+    };
 
-
-const base64payload = Buffer.from(JSON.stringify(payload)).toString("base64");
-const signature = crypto.createHmac("sha384", API_SECRET).update(base64payload).digest("hex");
-const headers = {
-    "Content-Type": "text/plain",
-    "Content-Length": "0",
-    "X-GEMINI-APIKEY": API_KEY,
-    "X-GEMINI-PAYLOAD": base64payload,
-    "X-GEMINI-SIGNATURE": signature,
-    "Cache-Control": "no-cache",
+    return headers;
 };
 
 export const gemini = async () => {
-    const response = await fetch(FULL_ENDPOINT, {
+    const accountInfoPromise = fetch(BASE_URL + PATHS.ACCOUNT_INFO, {
         method: "POST",
-        headers,
+        headers: getHeaders(PATHS.ACCOUNT_INFO),
     });
-    const json = await response.json();
-    console.log("json :>> ", json);
+
+    const accountBalancesPromise = fetch(BASE_URL + PATHS.ACCOUNT_BALANCES, {
+        method: "POST",
+        headers: getHeaders(PATHS.ACCOUNT_BALANCES),
+    });
+
+    const [accountInfoResponse, accountBalancesResponse] = await Promise.all([
+        accountInfoPromise,
+        accountBalancesPromise,
+    ]);
+
+    const accountInfo = await accountInfoResponse.json();
+    const accountBalances = await accountBalancesResponse.json();
+    console.log("accountInfo :>> ", accountInfo);
+    console.log("accountBalances :>> ", accountBalances);
 };
 
 // {
